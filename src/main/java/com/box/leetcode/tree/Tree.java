1abfe49715d3e4c5ad0255d9744a0bdd0159ff0d
WINDOWS-J31VKE5\hspcadmin <gbhello@foxmail.com>
2022-10-22 23:05:52 +0800

    初始化

--- src/main/java/com/box/leetcode/tree/Tree.java
+++ src/main/java/com/box/leetcode/tree/Tree.java
@@ -3,6 +3,7 @@ package com.box.leetcode.tree;
 import com.box.leetcode.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -17,6 +18,33 @@ public class Tree {
         Tree tree = new Tree();
         TreeNode root = TreeNode.init();
         System.out.println(tree.preorderTraversal(root));
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        ArrayList<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> list = new ArrayList<>();
+            int size = queue.size();
+
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            result.add(list);
+        }
+        return result;
     }
 
     public List<Integer> preorderTraversal(TreeNode root) {
