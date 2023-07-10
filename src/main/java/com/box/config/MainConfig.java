52abf29fa6403ddbc068e0036076c523d9a96a97
gengbin <gengbin29080@hundsun.com>
2023-07-10 14:52:47 +0800

    1

--- src/main/java/com/box/config/MainConfig.java
+++ src/main/java/com/box/config/MainConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 /**
- * 描述
+ * 描述1
  *
  * @author gengbin29080
  * @date 2022/1/7
